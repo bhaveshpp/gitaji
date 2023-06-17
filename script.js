497aa8fbddc7d7b117de5561d7425a2bee0bfa0a
bhaveshpp <bhaveshp.magento@gmail.com>
2023-06-17 22:48:14 +0530

    add to home action

--- script.js
+++ script.js
@@ -16,3 +16,63 @@ xhr.onerror = function() {
 };
 
 xhr.send();
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', event => {
+  // Prevent the default browser prompt
+  event.preventDefault();
+
+  // Store the event for later use
+  deferredPrompt = event;
+
+  // Show your custom add-to-home-screen popup
+  showAddToHomeScreenPopup();
+});
+
+function showAddToHomeScreenPopup() {
+  // Show your custom popup or banner here
+  // You can create an HTML element and style it as desired
+
+  const popup = document.getElementById('add-to-home-screen-popup');
+  popup.style.display = 'block';
+
+  const addButton = document.getElementById('add-button');
+  addButton.addEventListener('click', addToHomeScreen);
+
+  const cancelButton = document.getElementById('cancel-button');
+  cancelButton.addEventListener('click', cancelAddToHomeScreen);
+}
+
+function addToHomeScreen() {
+  // Show the browser's install prompt
+  deferredPrompt.prompt();
+
+  // Wait for the user's response
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+
+    // Reset the deferredPrompt variable
+    deferredPrompt = null;
+
+    // Hide the custom popup or banner
+    hideAddToHomeScreenPopup();
+  });
+}
+
+function cancelAddToHomeScreen() {
+  // Reset the deferredPrompt variable
+  deferredPrompt = null;
+
+  // Hide the custom popup or banner
+  hideAddToHomeScreenPopup();
+}
+
+function hideAddToHomeScreenPopup() {
+  const popup = document.getElementById('add-to-home-screen-popup');
+  popup.style.display = 'none';
+}
