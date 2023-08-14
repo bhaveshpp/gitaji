3d89795eb4eabe75bf0b05e429f192f7d9293234
bhaveshpp <bhaveshp.magento@gmail.com>
2023-08-14 08:48:01 +0530

    update

--- script.js
+++ script.js
@@ -1,78 +1,95 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/db.json', true);
-
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    const jsonData = JSON.parse(xhr.responseText);
-    // Access the data from the JSON file
-    console.log(jsonData);
-  } else {
-    console.error('Error fetching JSON file:', xhr.status);
-  }
-};
-
-xhr.onerror = function() {
-  console.error('Error fetching JSON file');
-};
-
-xhr.send();
+var css = document.querySelector(':root');
 
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', event => {
-  // Prevent the default browser prompt
-  event.preventDefault();
+window.addEventListener('beforeinstallprompt', event=>{
+    // Prevent the default browser prompt
+    event.preventDefault();
 
-  // Store the event for later use
-  deferredPrompt = event;
+    // Store the event for later use
+    deferredPrompt = event;
 
-  // Show your custom add-to-home-screen popup
-  showAddToHomeScreenPopup();
-});
+    // Show your custom add-to-home-screen popup
+    showAddToHomeScreenPopup();
+}
+);
 
 function showAddToHomeScreenPopup() {
-  // Show your custom popup or banner here
-  // You can create an HTML element and style it as desired
+    // Show your custom popup or banner here
+    // You can create an HTML element and style it as desired
 
-  const popup = document.getElementById('add-to-home-screen-popup');
-  popup.style.display = 'block';
+    const popup = document.getElementById('add-to-home-screen-popup');
+    popup.style.display = 'block';
 
-  const addButton = document.getElementById('add-button');
-  addButton.addEventListener('click', addToHomeScreen);
+    const addButton = document.getElementById('add-button');
+    addButton.addEventListener('click', addToHomeScreen);
 
-  const cancelButton = document.getElementById('cancel-button');
-  cancelButton.addEventListener('click', cancelAddToHomeScreen);
+    const cancelButton = document.getElementById('cancel-button');
+    cancelButton.addEventListener('click', cancelAddToHomeScreen);
 }
 
 function addToHomeScreen() {
-  // Show the browser's install prompt
-  deferredPrompt.prompt();
+    // Show the browser's install prompt
+    deferredPrompt.prompt();
 
-  // Wait for the user's response
-  deferredPrompt.userChoice.then(choiceResult => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
+    // Wait for the user's response
+    deferredPrompt.userChoice.then(choiceResult=>{
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+
+        // Reset the deferredPrompt variable
+        deferredPrompt = null;
+
+        // Hide the custom popup or banner
+        hideAddToHomeScreenPopup();
     }
-
+    );
+}
+function cancelAddToHomeScreen() {
     // Reset the deferredPrompt variable
     deferredPrompt = null;
 
     // Hide the custom popup or banner
     hideAddToHomeScreenPopup();
-  });
 }
-
-function cancelAddToHomeScreen() {
-  // Reset the deferredPrompt variable
-  deferredPrompt = null;
-
-  // Hide the custom popup or banner
-  hideAddToHomeScreenPopup();
-}
-
 function hideAddToHomeScreenPopup() {
-  const popup = document.getElementById('add-to-home-screen-popup');
-  popup.style.display = 'none';
+    const popup = document.getElementById('add-to-home-screen-popup');
+    popup.style.display = 'none';
+}
+function canOverlay(flag=true) {
+    if (flag) {
+        document.getElementById("overlay").style.display = 'block';
+    } else {
+        document.getElementById("overlay").style.display = 'none';
+    }
+
+}
+var sidebar = document.getElementById("mySidenav");
+function openNav() {
+    sidebar.style.width = "250px";
+    canOverlay(true);
+}
+
+function closeNav() {
+    if (sidebar.style.width != 0) {
+        document.getElementById("mySidenav").style.width = "0";
+        canOverlay(false);
+    }
+}
+
+function shareApp() {
+    if (navigator.share) {
+    navigator.share({
+        title: 'શ્રી ગીતાજી',
+        text: 'Guidance for life journey!',
+        url: 'https://bhaveshpp.github.io/gitaji/',
+      })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+  } else {
+    console.log('Share not supported on this browser, do it the old way.');
+  }
 }
